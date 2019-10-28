db44a9b0c98eaf42277fa1c699bc7df9b29627ee
Vent <ventaileron@gmail.com>
2019-10-28 16:17:27 +0100

    Full live?

--- copyFiles.sh
+++ copyFiles.sh
@@ -3,4 +3,6 @@
 cp ./base.html ../kryptedauth/app/core/templates/global/base.html 
 cp ./base.css ../kryptedauth/app/static/global/css/base.css
 cp ./sf.png ../kryptedauth/app/static/imgs/sf.png
+cp ./index.html ../kryptedauth/app/modules/frontpage/templates
+cp ./event_row.html ../kryptedauth/app/modules/engagement/templates/events
 
