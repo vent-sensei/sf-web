446949d11aeb95b1a65bfb1bd28516236f45db93
Vent <ventaileron@gmail.com>
2019-10-28 15:08:30 +0000

    Online testing

--- copyFiles.sh
+++ copyFiles.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-cp ../kryptedauth/app/core/templates/global/base.html ./
-cp ../kryptedauth/app/staticfiles/global/css/base.css ./
+cp ./base.html ../kryptedauth/app/core/templates/global/base.html 
+cp ./base.css ../kryptedauth/app/static/global/css/base.css
+cp ./sf.png ../kryptedauth/app/static/imgs/sf.png
+
