483effc719302ec2a4f19c5c2031276e0794d2f4
Vent <ventaileron@gmail.com>
2019-11-02 19:22:49 +0100

    Dashboard test

--- copyFiles.sh
+++ copyFiles.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cp ./base.html ../kryptedauth/app/core/templates/global/base.html 
+cp ./dashboard.html ../kryptedauth/app/core/templates/core
 cp ./base.css ../kryptedauth/app/static/global/css/base.css
 cp ./sf.png ../kryptedauth/app/static/imgs/sf.png
 cp ./index.html ../kryptedauth/app/modules/frontpage/templates
