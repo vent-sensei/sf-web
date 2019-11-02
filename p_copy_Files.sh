74afdd0b7b09f62954d2476645134284da7b17a0
Vent <ventaileron@gmail.com>
2019-11-02 19:43:07 +0100

    Test in prod

---
+++ p_copy_Files.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp ./base.html /var/kryptedauth/app/core/templates/global/base.html 
+cp ./dashboard.html /var/kryptedauth/app/core/templates/core
+cp ./base.css /var/kryptedauth/app/static/global/css/base.css
+cp ./sf.png /var/kryptedauth/app/static/imgs/sf.png
+cp ./index.html /var/kryptedauth/app/modules/frontpage/templates
+cp ./event_row.html /var/kryptedauth/app/modules/engagement/templates/events
+
